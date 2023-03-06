3a54b07f44678f47304903d5f7269166d36f36f1
orti10 <ortalh10@gmail.com>
2023-03-06 13:38:10 +0200

    update

--- websitesRecords/server/db.js
+++ websitesRecords/server/db.js
@@ -1,11 +1,19 @@
 const Pool = require("pg").Pool;
-
 const pool = new Pool({
   user: "postgres",
   password: "ortalh10",
   host: "localhost",
   port: 5432,
   database: "domainsrecords"
+
+  //I tried to write a more secure code but because of a library import problem it didn't work for me!
+  // The code is below
+
+  // user: process.env.DB_USER,
+  // password: `${process.env.DB_PASSWORD}`,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // database: process.env.DB_NAME,
 
 });
 
